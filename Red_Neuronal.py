355d85d48773420f98748aa57699600bd0b53148
Alex Zarate <zarate.alejandroM14@live.com.mx>
2022-04-02 18:28:32 -0600

    Version completa

--- Red_Neuronal.py
+++ Red_Neuronal.py
@@ -1,4 +1,3 @@
-import time
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import *
@@ -6,13 +5,8 @@ from tkinter.filedialog import askopenfilename
 import numpy as np
 import threading
 
-X = []
-W_hide = []
-W_out = []
-d = []
-
-colors = ['red', 'green']
-
+#---------------------------------------------------------------------------------------------------------
+#Cargar entradadas X
 def select_x_file():
     global X, d
 
@@ -37,7 +31,8 @@ def select_x_file():
     else:
         print("The number of patterns does not match the number of desired values.")
     
-
+#---------------------------------------------------------------------------------------------------------
+#Cargar valores deseados de las entradas
 def select_d_file():
     global  d, W
 
@@ -52,20 +47,21 @@ def select_d_file():
             d.append(int(lines[i]))
         x_file_button.config(state=NORMAL)
         print("The 'd' file has been loaded.")
-        initializeWeights()
         d = np.array(d)
     else:
         print("The 'd' file is empty.")
 
+#---------------------------------------------------------------------------------------------------------
+#Inicializar los pesos de las neuronas en funcion a cuantas neuronas haya
 def initializeWeights():
-    global W_hide
-    global W_out
-    W_hide = np.matrix(np.random.rand(2,3))
-    W_out = np.random.rand(3)
+    global W_hide, W_out, num_neurons
+    W_hide = np.matrix(np.random.rand(num_neurons.get(),3))
+    W_out = np.random.rand(num_neurons.get()+1)
 
+#---------------------------------------------------------------------------------------------------------
+#Reimprimir la grafica principal con su plano y tamaño correspondiente
 def print_axis():
-    global ax
-    #Imprimimos los ejes del plano cartesiano
+    global ax, ax_e
     ejeX = [-5,5]
     ejeY = [-5,5]
     zeros = [0,0]
@@ -74,6 +70,28 @@ def print_axis():
     plt.xlim(-1.5,1.5)
     plt.ylim(-1.5,1.5)
 
+#---------------------------------------------------------------------------------------------------------
+#Limpiar pantalla y valores
+def clean_screen():
+    global X, W_hide, W_out, d
+    X = []
+    d = []
+    W_hide = []
+    W_out = []
+
+    ax.cla()
+    ax_e.cla()
+    ax_e.set_xticklabels([])
+
+    print_axis()
+    canvas.draw()
+    canvas_e.draw()
+
+    x_file_button.config(state=DISABLED)
+    start_button.config(state=DISABLED)
+
+#---------------------------------------------------------------------------------------------------------
+#Funcion de activacion
 def ActivationFunc(x, w):
     global func_value, a_gui
     a = float(a_gui.get())
@@ -94,6 +112,8 @@ def ActivationFunc(x, w):
 
     return F_u
 
+#---------------------------------------------------------------------------------------------------------
+#Funcion de activacion derevada
 def ActivationFuncDerivated(Y):
     global func_value, a_gui
     a = float(a_gui.get())
@@ -112,11 +132,16 @@ def ActivationFuncDerivated(Y):
 
     return F_u
 
+#---------------------------------------------------------------------------------------------------------
+#Clasificar datos
 def dataClasification():
     global eta_gui, epoch_gui, X, W_hide, W_out, d
     eta = float(eta_gui.get())
     epoch = int(epoch_gui.get())
+    epoch_cont = 0
     error = True
+    error_graph = []
+    initializeWeights()
 
     while epoch and error:
         error = False
@@ -126,7 +151,6 @@ def dataClasification():
         salida = ActivationFunc(X_hide, np.array(W_out).flatten())
         errors = d - salida
 
-#-----------------------------------------------------------------------------------------------------------
         #capa salida
         delta_out = []
         for i in range(len(X_hide)):
@@ -142,10 +166,11 @@ def dataClasification():
                 W_hide[j] = W_hide[j] + np.dot(X[i],eta*delta_hide)
 
         ax.cla()
-#---------------------------------------------------------------------------------------------------------
+
         square_error =  np.average(np.power(errors,2))
         if square_error > float(min_error.get()):
             error = True
+        error_graph.append(square_error)
 
         #Imprimimos los puntos en la grafica
         salida_oculta = ActivationFunc(X, np.transpose(W_hide))
@@ -157,11 +182,6 @@ def dataClasification():
                 ax.plot(X[i,1],X[i,2],'o', color='green')
             else:
                 ax.plot(X[i,1],X[i,2],'o', color='red')
-
-        print(square_error)
-        print("----------------",epoch,"-------------")
-
-#---------------------------------------------------------------------------------------------------------
 
         x_v = np.linspace(-1.5, 1.5, 20)
         y_v = np.linspace(-1.5, 1.5, 20)
@@ -178,35 +198,44 @@ def dataClasification():
             Z_m.append(np.array(salida).flatten())
         ax.contourf(X_m, Y_m, Z_m, 0)
 
-#---------------------------------------------------------------------------------------------------------
+        ax_e.cla()
+        ax_e.plot(error_graph)
+        ax_e.set_xticklabels([])
+        
         epoch-=1
+        epoch_cont+=1
         print_axis()
         canvas.draw()
+        canvas_e.draw()
 
-def clean_screen():
-    global X, W, d
-    X = []
-    d = []
-    ax.cla()
-    print_axis()
-    canvas.draw()
-    W = []
-    x_file_button.config(state=DISABLED)
-    start_button.config(state=DISABLED)
+        error_actual.config(text="Error: "+str(square_error))
+        epoch_count.config(text="Epoca: "+str(epoch_cont))
     
+#---------------------------------------------------------------------------------------------------------
+#Inicio de programa + interfaz
+X = []
+W_hide = []
+W_out = []
+d = []
+
+colors = ['red', 'green']
 
 #Inizializamos la grafica de matplotlib
+fig_e, ax_e= plt.subplots(facecolor='#8D96DA')
+ax_e.set_xticklabels([])
 fig, ax= plt.subplots(facecolor='#8D96DA')
 print_axis()
 
 mainwindow = Tk()
-mainwindow.geometry('750x600')
-mainwindow.wm_title('Perceptron')
+mainwindow.geometry('910x600')
+mainwindow.wm_title('Red neuronal multicapa')
 eta_gui = StringVar(mainwindow, 0)
 epoch_gui = StringVar(mainwindow, 0)
 a_gui = StringVar(mainwindow, 0)
 min_error = StringVar(mainwindow, 0)
 func_value = IntVar(mainwindow, 0)
+num_neurons = IntVar(mainwindow, 0)
+
 #Colocamos la grafica en la interfaz
 canvas = FigureCanvasTkAgg(fig, master = mainwindow)
 canvas.get_tk_widget().place(x=10, y=10, width=580, height=580)
@@ -218,38 +247,52 @@ a_entry = Entry(mainwindow, textvariable=a_gui)
 a_entry.place(x=600, y=80)
 
 Eta_label = Label(mainwindow, text = "Eta: ")
-Eta_label.place(x=600, y=110)
+Eta_label.place(x=750, y=60)
 Eta_entry = Entry(mainwindow, textvariable=eta_gui)
-Eta_entry.place(x=600, y=130) 
+Eta_entry.place(x=750, y=80) 
 
 Epoch_label = Label(mainwindow, text = "Num. Epoch: ")
-Epoch_label.place(x=600, y=160)
+Epoch_label.place(x=600, y=110)
 Epoch_entry = Entry(mainwindow, textvariable=epoch_gui)
-Epoch_entry.place(x=600, y=180)
+Epoch_entry.place(x=600, y=130)
 
 error_label = Label(mainwindow, text = "Min. Error: ")
-error_label.place(x=600, y=210)
+error_label.place(x=750, y=110)
 error_entry = Entry(mainwindow, textvariable=min_error)
-error_entry.place(x=600, y=230)
+error_entry.place(x=750, y=130)
+
+neurons_label = Label(mainwindow, text = "Num. Neuronas: ")
+neurons_label.place(x=680, y=160)
+neurons_entry = Entry(mainwindow, textvariable=num_neurons)
+neurons_entry.place(x=680, y=180)
 
 start_button = Button(mainwindow, text="Go!", command=lambda:threading.Thread(target=dataClasification).start(), state=DISABLED)
-start_button.place(x=600, y=260)
+start_button.place(x=600, y=210)
 
 x_file_button = Button(mainwindow, text="Select X file", command=select_x_file, state=DISABLED)
-x_file_button.place(x=600, y=290)
+x_file_button.place(x=600, y=240)
 
 d_file_button = Button(mainwindow, text="Select D file", command=select_d_file)
-d_file_button.place(x=600, y=320)
+d_file_button.place(x=600, y=270)
 
 clean_button = Button(mainwindow, text="Clean", command=clean_screen)
-clean_button.place(x=600, y=350)
+clean_button.place(x=600, y=300)
 
 logistica_rb = Radiobutton(mainwindow, text="Logistica", variable=func_value, value=0)
-logistica_rb.place(x=600, y=380)
+logistica_rb.place(x=600, y=330)
 tangente_rb = Radiobutton(mainwindow, text="Tangente", variable=func_value, value=1)
-tangente_rb.place(x=600, y=400)
+tangente_rb.place(x=600, y=350)
 Lineal_rb = Radiobutton(mainwindow, text="Lineal", variable=func_value, value=2)
-Lineal_rb.place(x=600, y=420)
+Lineal_rb.place(x=600, y=370)
+
+epoch_count = Label(mainwindow, text="Epoca: 0")
+epoch_count.place(x=600, y=420)
+
+error_actual = Label(mainwindow, text="Error: 0")
+error_actual.place(x=700, y=420)
+
+canvas_e = FigureCanvasTkAgg(fig_e, master = mainwindow)
+canvas_e.get_tk_widget().place(x=600, y=460, width=300, height=130)
 
 #Mostramos la interfaz
 mainwindow.mainloop()
